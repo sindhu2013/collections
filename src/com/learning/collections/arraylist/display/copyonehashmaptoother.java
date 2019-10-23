d80fa4b03c3ab65ab44f712d092c1cfa0ce1e677
Vijay Mudunuri <sindu2013>
2019-10-23 17:06:47 -0400

    with '#' will be ignored, and an empty message aborts the commit.

--- src/com/learning/collections/arraylist/display/copyonehashmaptoother.java
+++ src/com/learning/collections/arraylist/display/copyonehashmaptoother.java
@@ -16,7 +16,11 @@ package com.learning.collections.arraylist.display;
 	     System.out.println("Key 2 exists in HashMap? : " + flag);
 	     boolean flag1= hmap.containsKey(5);
 	     System.out.println("Key 5 exists in HashMap? : " + flag1);
+	     boolean flag3 = hmap.containsValue("AA");
+	     System.out.println("Key 2 exists in HashMap? : " + flag3);
 	  
+	     String val=hmap.get(4);
+	     System.out.println("The Value mapped to Key 4 is:"+ val);
 	     // Create another HashMap
 	     HashMap<Integer, String> hmap2 = new HashMap<Integer, String>();
 	 
@@ -34,6 +38,9 @@ package com.learning.collections.arraylist.display;
 	     // Displaying HashMap "hmap2" content
 	     System.out.println("HashMap 2 contains: "+ hmap2);
 	     System.out.println("HashMap 3 contains: "+ hmap3);
+	     HashMap<Integer, String> hmap4 = new HashMap<Integer, String>();
+	     hmap4=(HashMap)hmap3.clone();
+	     System.out.println("HashMap 4 contains: "+ hmap4);
 	  }
 	
 }
